9f335248b2b2ed1af1e8dedb9e22c017565ea2d8
Jas Shroff <62849970+jasshroff@users.noreply.github.com>
2026-08-22 00:45:10 +0530

    Time: 14 ms (28.8%), Space: 21 MB (50.21%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -8,10 +8,10 @@ class Solution:
 
         for i in range(n-1, -1, -1):
             if abs(nums[left]) > abs(nums[right]):
-                result[i] = nums[left] * nums[left]
+                result[i] = nums[left] ** 2
                 left += 1
             else:
-                result[i] = nums[right] * nums[right]
+                result[i] = nums[right] ** 2
                 right -= 1
         return result
         
